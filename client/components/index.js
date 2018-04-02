2fb9aa021d4b13b7371215bee04445eea328366f
jakeBergs <jake.bergal@gmail.com>
2018-04-02 16:38:38 -0500

    created saved counter store for redux stores

--- client/components/index.js
+++ client/components/index.js
@@ -4,6 +4,6 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
+export {default as UserHome} from './UserHome'
 export {Login, Signup} from './auth-form'
 export {default as AddThings} from './AddThings'

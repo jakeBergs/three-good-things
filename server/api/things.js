7d582eebefee72ba018f0d60e21966151efd931c
jbergal <jake.bergal@gmail.com>
2018-05-08 14:30:39 -0500

    added in new page and reducer to handle it

--- server/api/things.js
+++ server/api/things.js
@@ -4,6 +4,19 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   Thing.findAll()
-    .then(things => res.send(things))
+    .then(things => res.json(things))
+    .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+  Thing.findById(req.params.id)
+    .then(thing => res.json(thing))
+    .catch(next);
+})
+
+router.put('/:id', (req, res, next) => {
+  Thing.findById(req.params.id)
+    .then(thing => thing.update({description: req.body.description}))
+    .then(update => res.json(update))
     .catch(next)
 })
